26aba5026fbfe1edf17fda57ab15d61942474afe
tejas <tejasa2@illinois.edu>
2020-03-01 15:55:19 -0600

    Create mc_probability function in main
    Samples multiple trials and returns probability

--- src/main.py
+++ src/main.py
@@ -14,15 +14,25 @@ a0 = 0.0529
 
 # r: 0 -> 10*a0, theta: 0-> pi, phi: 0 -> 2pi
 def check_prob(r, theta, phi, function):
-    num = 1000
-    for i in range(num):
-        # Create point and define x, y, z to be randoms within the proper range
-        n = point()
-        n.r = random.uniform(0, 10 * a0)
-        n.theta = random.uniform(0, math.pi)
-        n.phi = random.uniform(0, 2 * math.pi)
+    # Create point and define x, y, z to be randoms within the proper range
+    n = point()
+    n.r = random.uniform(0, 10 * a0)
+    n.theta = random.uniform(0, math.pi)
+    n.phi = random.uniform(0, 2 * math.pi)
 
-        return function(n.r, n.theta, n.phi) >= .85*max(function)
+    return function(n.r, n.theta, n.phi) >= .85*max(function)
+
+
+# Takes a number of trials and a function, and checks how many random points are above max threshold
+# Using check_prob
+def mc_probability(trials, function):
+    counter = 0
+    for i in range(trials):
+        # Fix additional zeros once check_prob is fully defined
+        if check_prob(0, 0, 0, function):
+            counter += 1
+
+    return counter / trials
 
 
 class point:
