6c224896b923b48829a0dba26257527b29dd9de3
tejas <tejasa2@illinois.edu>
2020-03-03 21:13:54 -0600

    Format some comments and reorder code more logically

--- src/main.py
+++ src/main.py
@@ -24,7 +24,6 @@ a0 = 0.0529
 # Wave function for (n, l, m) = (1, 0, 0)
 def calcP1S0(r, theta, phi):
     psi = abs((1 / (math.sqrt(math.pi) * (a0 ** (3 / 2)))) * math.e ** (-r / a0))
-    #print(psi)
     p = psi ** 2 * 4 * math.pi * r ** 2
     return p
 
@@ -32,18 +31,16 @@ def calcP1S0(r, theta, phi):
 # Wave function for (n, l, m) = (2, 0, 0)
 def calcP2S0(r, theta, phi):
     psi = abs((1 / (4 * math.sqrt(2 * math.pi)) * a0 ** (3 / 2)) * (2 - r / a0) * math.e ** (-r / (2 * a0)))
-    #print(psi)
     p = psi ** 2 * 4 * math.pi * r ** 2
-    return (p * 10**8 * 3.61 / 7.911)
+    return p * 10 ** 8 * 3.61 / 7.911
 
 
 # Wave function for (n, l, m) = (2, 1, 0)
 def calcP2P0(r, theta, phi):
     psi = abs(
         (1 / (4 * math.sqrt(2 * math.pi)) * a0 ** (3 / 2)) * (r / a0) * math.e ** (-r / (2 * a0)) * math.cos(theta))
-    #print(psi)
     p = psi ** 2 * 2 * math.sin(theta) * math.pi * r ** 2
-    return (p *10**8 * 1.9586 / 4.67270677)
+    return p * 10 ** 8 * 1.9586 / 4.67270677
 
 
 # Print statements to verify values
@@ -90,9 +87,9 @@ plt.show()
 
 a0 = 0.0529
 solns = []
+max1S0 = 10.233
 
 radii = sympy.solve((-1.02157 * 10 ** 6 * math.e ** (-37.8072 * r) * (r ** 2 - 0.0529 * r)), r)
-max1S0 = 10.233
 for radius in radii:
     if max1S0 < calcP1S0((complex(radius)).real, 0, 0):
         max1S0 = calcP1S0((complex(radius)).real, 0, 0)
@@ -101,6 +98,17 @@ print("Max1s0 %s" % max1S0)
 #MANUAL:
 max2S0 = 3.6100  # at 3a0 + a0/(0.2*sqrt(5))
 max2P0 = 1.9585  # at 4a0, pi/4 or 3pi/4
+
+
+# Class point to store (r, theta, phi) coordinates
+class spherical_point:
+    def __init__(self):
+        self.r = 0
+        self.theta = 0
+        self.phi = 0
+
+    def __str__(self):
+        return "(%s, %s, %s)" % (self.r, self.theta, self.phi)
 
 
 # r: 0 -> 10*a0, theta: 0-> pi, phi: 0 -> 2pi
@@ -117,25 +125,17 @@ def is_above_max(rmax, thetamax, phimax, threshold):
 
 # Takes a number of trials and a function, and checks how many random points are above max threshold
 # Using check_prob
+
+# #####NOTE#####
+# This function is totally unnecessary. Wenever use the return value and it can be replaced by a for loop of
+# is_above_max
 def mc_probability(trials):
     counter = 0
     for i in range(trials):
-        # Fix additional zeros once check_prob is fully defined
         if is_above_max(10 * a0, math.pi, 2 * math.pi, 0.99):
             counter += 1
 
     return counter / trials
-
-
-# Class point to store (r, theta, phi) coordinates
-class spherical_point:
-    def __init__(self):
-        self.r = 0
-        self.theta = 0
-        self.phi = 0
-
-    def __str__(self):
-        return "(%s, %s, %s)" % (self.r, self.theta, self.phi)
 
 
 mc_probability(100000)
